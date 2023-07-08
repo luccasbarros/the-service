5c651b2be41c2ab789c57303d9353a78a94bc44c
Luccas Barros <luccasbarros@pm.me>
2023-07-08 12:00:02 -0300

    feat: add errors handling

---
+++ pkg/errors/errors.go
@@ -0,0 +1,35 @@
+package errors
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+const (
+	InternalServerError = "Internal Server Error"
+)
+
+type ErrorResponse struct {
+	Message    string `json:"error"`
+	StatusCode int    `json:"status"`
+}
+
+func RespondError(w http.ResponseWriter, statusCode int, message string) {
+	errorResponse := &ErrorResponse{
+		Message:    message,
+		StatusCode: statusCode,
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(statusCode)
+
+	errJSON, err := json.Marshal(errorResponse)
+	if err != nil {
+		panic(err)
+	}
+
+	_, err = w.Write(errJSON)
+	if err != nil {
+		panic(err)
+	}
+}
