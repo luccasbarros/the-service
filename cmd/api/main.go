168bb262e5699f2e18c9cf8b2d428f365cc87ff8
Luccas Barros <luccasbarros@pm.me>
2023-08-01 10:47:10 -0300

    fix: name convention; my dumb implementation of server

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -15,7 +15,7 @@ type API struct {
 	logger *log.Logger
 }
 
-func NewApi() *API {
+func NewAPI() *API {
 	db, err := data.InitPool()
 	if err != nil {
 		log.Fatalf("init pool error %v", err)
@@ -32,15 +32,19 @@ func NewApi() *API {
 		Addr:              "localhost:8080",
 		ReadTimeout:       10 * time.Second,
 		ReadHeaderTimeout: 5 * time.Second,
+		WriteTimeout:      15 * time.Second,
 	}
 
 	return apiInstance
 }
 
 func main() {
-	api := NewApi()
+	api := NewAPI()
 
 	api.logger.Println("Server is running on http://localhost:8080")
 
-	http.ListenAndServe(api.server.Addr, api.server.Handler)
+	err := api.server.ListenAndServe()
+	if err != nil {
+		api.logger.Fatalf("Error starting the server %v ", err)
+	}
 }
